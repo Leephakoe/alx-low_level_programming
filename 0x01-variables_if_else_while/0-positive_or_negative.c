0b5dd9109563cadb92c6c6b4f567dc1c7fb6839b
Leephakoe <leephakoe@gmail.com>
2022-09-09 10:06:54 -0700

    Positive anything

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+ 
+
+int main () {
+
+
+
+	   /* local variable definition */
+
+	   int n = 0;
+
+	    
+
+	      /* check the boolean condition */
+
+	      if( n < 0 ) {
+
+		            /* if condition is true then print the following */
+
+		            printf("n is less than 20\n" );
+
+			       } else {
+
+				             /* if condition is false then print the following */
+				             printf("n is not less than 20\n" );
+
+					        }
+
+	         
+
+	         printf("value of n is : %d\n", a);
+
+		  
+
+		    return 0;
+
+}
