a8c2dce073bf6a1016ee3442acd98126c0d5ae57
Leephakoe <leephakoe@gmail.com>
2022-09-17 17:54:41 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 /**
- * main-Entry point
- *
- * Description : prints "with proper grammar, but the outcome is a piece of art,\n");
- *
- * Return : Always 0 (Success)
+ *  main - print if the number is postive, zero, or negative
+ *  Description: using the main function this program prints "Programming is positive, zero, or negative
+ *  Return: 0
  */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
