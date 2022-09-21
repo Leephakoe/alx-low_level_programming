7108a7007a14ec8f81fac904ebde452478e4c90f
Leephakoe <leephakoe@gmail.com>
2022-09-21 11:53:04 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,24 @@
 #include "main.h" 
 
 /**
- *_strncat - Concatenates two strings using at most  an inputted number of bytes from src.
- *@dest: The string to be appended upon. 
- *@src: The string to be appended to dest.
- *@n: The number of bytes from src to be appended to dest.
- *Return: A pointer to the resulting string dest. 
+ *_strncat -> this is a function strcat
+ *@: first param
+ *src: second param
+ *Reruen: a string 
  */
-
 char *_strncat(char *dest, char *src, int n) 
 {
-	 int index = 0, dest_len = 0; 
+	 int len =0,i; 
 
-	  while (dest[index++])
-		  dest_len++;
+	  while (dest[len])
+		  len++;
 
-	   for (index = 0; src[index] && index < n; index++)
-		    dest[dest_len++] = src[index]; 
+	   for (i=0 ; src[i] ! = '\0' ; i++)
+	   {
+		    dest[len] = src[i];
+		    len + = 1;
+	   }
+	   dest+[len] ='\0'
 
 	    return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,11 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_H  
-
-/**
- * File: main.h 
- *  Auth: Leephakoe
- *  Desc: Header file containing prototypes for all functions used in the 0x06. C - More pointers, arrays and strings directory
- */
+#define MAIN_H
 
 int _putchar(char c); 
 char *_strcat(char *dest, char *src)
@@ -22,5 +16,3 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size); 
 
 #endif
-
-
