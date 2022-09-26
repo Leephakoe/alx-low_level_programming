0d151e4693ef69231257eddf1c85a062dc5edf89
Leephakoe <leephakoe@gmail.com>
2022-09-26 12:01:05 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,11 +2,14 @@
 #define MAIN_H
 
 /**
- *
+ *File: main.h
  * Description: Header file containing all prototype solutions
  * used in the 0x07-pointers_arrays_strings
  */
 
+#include <stddef.h> 
+
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
