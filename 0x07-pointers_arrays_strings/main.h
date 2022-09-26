1b22322267a8815aa7b76fd0f71f0d8c7158cb6b
Leephakoe <leephakoe@gmail.com>
2022-09-26 13:32:35 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,14 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- *File: main.h
- * Description: Header file containing all prototype solutions
- * used in the 0x07-pointers_arrays_strings
- */
-
-#include <stddef.h> 
-
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -19,5 +11,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
 #endif
