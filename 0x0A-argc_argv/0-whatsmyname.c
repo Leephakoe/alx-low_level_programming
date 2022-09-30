a7cabbb2f7c08c8ab2c0069e4bc43246490bdae3
Leephakoe <leephakoe@gmail.com>
2022-09-30 05:24:33 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print name of file
+ * @argc:  count of arguments
+ * @argv: array of arguments
+ * Return: 1
+ */
+
+int main(__attribute__((unused)) int argc,
+		{
+		printf("%s\n", argv[0]);
+
+		return (0);
+		}
