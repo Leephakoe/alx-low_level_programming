e333a09c9cc65313780509fc2b61349025bad4dd
Leephakoe <leephakoe@gmail.com>
2022-10-03 12:36:21 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,23 +1,22 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
-
 /**
- * malloc_checked - function that allocates memory using malloc.
- *
- * @b: This is the length to allocate in the memory
- *
- * Return: Returns a pointer to the allocated memory
- * if malloc fails, the malloc_checked function should cause normal process
- * termination with a status value of 98
- *
+ *  create_array -create the initialize a char array
+ *  @size: the size of the array to create
+ *  @c: initial value to fill array
+ *  Return: pointer to the created array
  */
-void *malloc_checked(unsigned int b)
+char *create_array(unsigned int size, char c)
 {
-	void *p;
+	unsigned int i;
+	char *p = NULL;
 
-	p = malloc(b);
-	if (p == NULL)
-		exit(98);
+	if (size > 0)
+		p = malloc(size);
+	if (p != NULL)
+	{
+		for (i = 0; i < size; i++)
+			p[i] = c;
+	}
+
 	return (p);
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,5 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
